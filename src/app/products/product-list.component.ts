0f0ec9e4018ad13c79c072aab3592cb8626ae25b
simonojok19 <simonojok19@gmail.com>
2021-09-11 15:37:18 +0300

    created the pipe for data transformations before display

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -36,7 +36,7 @@ export class ProductListComponent implements OnInit {
   ];
 
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    console.log('ngOnInit');
   }
 
   toggleImage(): void {
