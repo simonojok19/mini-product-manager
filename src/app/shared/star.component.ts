b6e9093934886d2f3dfe97460acc3284e45f7ef6
simonojok19 <simonojok19@gmail.com>
2021-09-11 16:19:45 +0300

    created star component

---
+++ src/app/shared/star.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnChanges, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'pm-star',
+  templateUrl: './star.component.html',
+  styleUrls: ['./star.component.css'],
+})
+export class StarComponent implements OnChanges {
+  rating: number = 4;
+  cropWidth: number = 75;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Changes ', changes);
+    this.cropWidth = this.rating * (75 / 5);
+  }
+}
