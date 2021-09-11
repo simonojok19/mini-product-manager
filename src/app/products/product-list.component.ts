046c9f4ba0dfc0262e2163a7927180eb55e038c4
simonojok19 <simonojok19@gmail.com>
2021-09-11 15:50:13 +0300

    using setters

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -11,7 +11,16 @@ export class ProductListComponent implements OnInit {
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
-  listFilter: string = 'cart';
+  private _listFilter: string = '';
+
+  get listFilter(): string {
+    return this._listFilter;
+  }
+
+  set listFilter(value: string) {
+    this._listFilter = value;
+    console.log('In setter', value);
+  }
   products: IProduct[] = [
     {
       productId: 1,
@@ -36,6 +45,7 @@ export class ProductListComponent implements OnInit {
   ];
 
   ngOnInit(): void {
+    this._listFilter = '';
     console.log('ngOnInit');
   }
 
