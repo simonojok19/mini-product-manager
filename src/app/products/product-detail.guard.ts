74587a06d88fe18fbf0128f6e9c94c2d7bb9c5c3
simonojok19 <simonojok19@gmail.com>
2021-09-12 06:36:54 +0300

    added the router gurads

--- src/app/products/product-detail.guard.ts
+++ src/app/products/product-detail.guard.ts
@@ -1,15 +1,28 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductDetailGuard implements CanActivate {
+  constructor(private router: Router) {}
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    state: RouterStateSnapshot
+  ): boolean {
+    const id = Number(route.paramMap.get('id'));
+    if (isNaN(id) || id < 1) {
+      alert('Invalid product id');
+      this.router.navigate(['/products']).then();
+      return false;
+    }
     return true;
   }
-  
 }
